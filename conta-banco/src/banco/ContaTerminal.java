721dbd9b060ead3a79088f8b903237c6915fe1ae
Mattheus2403 <matheusmoraidois@gmail.com>
2023-09-13 20:57:16 -0300

    docs: add TODO in class ContaTerminal.java

--- conta-banco/src/banco/ContaTerminal.java
+++ conta-banco/src/banco/ContaTerminal.java
@@ -3,7 +3,13 @@ package banco;
 public class ContaTerminal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO importar a classe scanner
+		
+		// Exibir as mensagens para o usuário
+		
+		// Obter pelo scanner valores digitados no terminal
+		
+		// Exibir a mensagem de conta criada
 
 	}
 
