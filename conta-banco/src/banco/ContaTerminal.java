7590812aaa9446dbc7c483254d895917f42e053f
Mattheus2403 <matheusmoraidois@gmail.com>
2023-09-13 21:30:50 -0300

    feat: set variables and input then in ContaTerminal.java

--- conta-banco/src/banco/ContaTerminal.java
+++ conta-banco/src/banco/ContaTerminal.java
@@ -12,6 +12,26 @@ public class ContaTerminal {
 		// Obter pelo scanner valores digitados no terminal
 		
 		// Exibir a mensagem de conta criada
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		//pede para o cliente digitar o número da agência de 4 digitos
+		System.out.println("Por favor, digite o número da conta: ");
+		int numero = scanner.nextInt();
+		
+		//pede para o cliente digitar seu nome
+		System.out.println("Por favor, digite seu nome: ");
+		String nomeCliente = scanner.next();
+		
+		//pede para o cliente digitar o número da agência de 4 digitos com final separado
+		System.out.println("Por favor, digite o número da agência: ");
+		String agencia = scanner.next();
+		
+		//pede para o cliente informar seu saldo 
+		System.out.println("Por favor, digite o seu saldo: ");
+		double saldo = scanner.nextDouble();
+		
+		
 
 	}
 
