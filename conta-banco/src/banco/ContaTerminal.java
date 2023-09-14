e20de2b90e7cd3f76007af1fcb443fcec4f8c319
Mattheus2403 <matheusmoraidois@gmail.com>
2023-09-13 22:08:29 -0300

    feat: add message of account creation and import java.util.locale in ContaTerminal.java

--- conta-banco/src/banco/ContaTerminal.java
+++ conta-banco/src/banco/ContaTerminal.java
@@ -2,6 +2,8 @@ package banco;
 
 import java.util.Scanner;
 
+import java.util.Locale;
+
 public class ContaTerminal {
 
 	public static void main(String[] args) {
@@ -13,25 +15,28 @@ public class ContaTerminal {
 		
 		// Exibir a mensagem de conta criada
 		
-		Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 		
-		//pede para o cliente digitar o número da agência de 4 digitos
+		//pede para o cliente digitar o número da conta de 4 digitos
 		System.out.println("Por favor, digite o número da conta: ");
-		int numero = scanner.nextInt();
+		int numeroConta = scanner.nextInt();
 		
 		//pede para o cliente digitar seu nome
 		System.out.println("Por favor, digite seu nome: ");
 		String nomeCliente = scanner.next();
 		
-		//pede para o cliente digitar o número da agência de 4 digitos com final separado
+		//pede para o cliente digitar o número da agência de 4 digitos com final separado por "-"
 		System.out.println("Por favor, digite o número da agência: ");
 		String agencia = scanner.next();
 		
 		//pede para o cliente informar seu saldo 
 		System.out.println("Por favor, digite o seu saldo: ");
-		double saldo = scanner.nextDouble();
+		Double saldo = scanner.nextDouble();
 		
-		
+		//mensagem da conclusão da criação da conta
+		System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso "
+				+ "banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo "
+						+ saldo + "R$ já está disponível para saque.");
 
 	}
 
